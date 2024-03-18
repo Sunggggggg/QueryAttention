25602a32a81dbadb15abfdad6f5f7bdf3d449a0a
Sunggggggg <sky990319@gmail.com>
2024-03-18 12:44:31 +0900

    f

--- scripts/eval_realestate10k.sh
+++ scripts/eval_realestate10k.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-python experiment_scripts/eval_realestate10k.py --experiment_name vis_realestate --batch_size 2 --gpus 2 --views 2 --checkpoint_path realestate_query_1/64_2_512/checkpoints/model_current.pth
+data_dirs=("Exp4" "Exp3_8queries" "Exp3_no_contra" "Exp3" "Exp2")
+
+for dir in $data_dirs
+do
+    ckpt_path=output/${dir}/64_8_512/checkpoints/model_current.pth
+    python experiment_scripts/eval_realestate10k.py --experiment_name vis_realestate --batch_size 2 --gpus 2 --views 2 --checkpoint_path ${ckpt_path}
+done
+
