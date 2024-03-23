19719e31cb1e3b0b417f9f5d67e57057e5ca7dbc
Sunggggggg <sky990319@gmail.com>
2024-03-23 18:18:59 +0900

    f

--- queries_visualization.py
+++ queries_visualization.py
@@ -148,8 +148,8 @@ if __name__ == "__main__" :
             for k in range(high_feat.shape[1]) :
                 query1, query2 = model.encoder.query1, model.encoder.query2 # [1, 100, 256]
                 tsne = TSNE(n_components=2, random_state=1)
-                query1_tsne = tsne.fit_transform(query1[0])
-                query2_tsne = tsne.fit_transform(query2[0])
+                query1_tsne = tsne.fit_transform(query1[0].cpu().numpy())
+                query2_tsne = tsne.fit_transform(query2[0].cpu().numpy())
                 color = np.arange(query2.shape[1])
                 plt.scatter(query1_tsne[:, 0], query1_tsne[:, 1], c=color)
                 writer.add_figure(f'query1_embedding{k}', plt.gcf(), total_iter)
